71a3bbfe82a8b9bd2a730603a28e8e5440bfd6e0
Xia Huyun <huyun.xia@nokia-sbell.com>
2023-05-13 23:24:23 +0800

    Add pivot root for minidocker

--- docker/internal/runc/container/container.go
+++ docker/internal/runc/container/container.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"syscall"
 
@@ -34,6 +35,57 @@ func NewParentProcess(tty bool) (*exec.Cmd, *os.File) {
 	return cmd, writePipe
 }
 
+func pivotRoot(root string) error {
+	if err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, ""); err != nil {
+		return err
+	}
+
+	// Mount the new root as a new filesystem
+	if err := syscall.Mount(root, root, "", syscall.MS_BIND|syscall.MS_PRIVATE, ""); err != nil {
+		return fmt.Errorf("mount rootfs to itself failed: %v", err)
+	}
+
+	// Create a new directory for the old root
+	pivotDir := filepath.Join(root, ".pivot_root")
+	if err := os.Mkdir(pivotDir, 0700); err != nil {
+		return err
+	}
+
+	// Pivot the root directory
+	if err := syscall.PivotRoot(root, pivotDir); err != nil {
+		return fmt.Errorf("pivot_root %v", err)
+	}
+
+	// Change the current working directory to "/"
+	if err := syscall.Chdir("/"); err != nil {
+		return fmt.Errorf("chdir %v", err)
+	}
+
+	// Unmount the old root
+	pivotDir = filepath.Join("/", ".pivot_root")
+	if err := syscall.Unmount(pivotDir, syscall.MNT_DETACH); err != nil {
+		return fmt.Errorf("umount pivot_root dir %v", err)
+	}
+
+	return os.Remove(pivotDir)
+}
+
+func setupMount() {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Errorf("get current location failed: %v", err)
+	}
+
+	log.Infof("current locaion: %v", pwd)
+	if err := pivotRoot(pwd); err != nil {
+		log.Errorf("change root filesystem failed: %v", err)
+	}
+
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	syscall.Mount("tmpfs", "/dev", "tmpfs", syscall.MS_NOSUID|syscall.MS_STRICTATIME, "mode=755")
+}
+
 func readUserCommands() []string {
 	pipe := os.NewFile(uintptr(3), "pipe")
 	msg, err := io.ReadAll(pipe)
@@ -51,8 +103,7 @@ func RunContainerInitProcess() error {
 		return fmt.Errorf("get empty command")
 	}
 
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	setupMount()
 
 	command, err := exec.LookPath(commands[0])
 	if err != nil {
