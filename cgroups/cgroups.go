956018da63a9b464f0c212034eb9efd56a6cfca3
Xia Huyun <huyun.xia@nokia-sbell.com>
2023-06-30 14:38:52 +0800

    refactor cgroups

--- cgroups/cgroups.go
+++ cgroups/cgroups.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -10,41 +9,61 @@ import (
 	"syscall"
 )
 
-const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+const (
+	exeCommand                 = "/proc/self/exe"
+	cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
+	isolateNamespace uintptr = syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS
+	localNamespace   uintptr = 0
+)
+
+var (
+	startupCommand = "sh"
+	startupArgs    = []string{"-c", `stress --vm-bytes 200m --vm-keep -m 1`}
+)
+
+func newCommand(namespace uintptr, name string, args ...string) *exec.Cmd {
+	cmd := exec.Command(name, args...)
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: namespace}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd
+}
+
+func runCommandInNamespace(namespace uintptr, name string, args ...string) error {
+	cmd := newCommand(namespace, name, args...)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func isRunExe(args []string, name string) bool {
+	return args[0] == name
+}
+
+func setCgroupFor(pid int, cgroupPath string) {
+	os.Mkdir(path.Join(cgroupPath, "testmemorylimit"), 0755)
+	ioutil.WriteFile(path.Join(cgroupPath, "testmemorylimit", "tasks"), []byte(strconv.Itoa(pid)), 0644)
+	ioutil.WriteFile(path.Join(cgroupPath, "testmemorylimit", "memory.limit_in_bytes"), []byte("500m"), 0644)
+}
 
 func main() {
-	fmt.Println(os.Args)
-	if os.Args[0] == "/proc/self/exe" {
-		fmt.Println("Current pid: ", syscall.Getpid())
-		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
-		cmd.SysProcAttr = &syscall.SysProcAttr{}
-		cmd.Stdin = os.Stdin
-		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-
-		if err := cmd.Run(); err != nil {
-			fmt.Println(err)
+	if isRunExe(os.Args, exeCommand) {
+		if err := runCommandInNamespace(localNamespace, startupCommand, startupArgs...); err != nil {
 			os.Exit(1)
 		}
 	}
 
-	cmd := exec.Command("/proc/self/exe")
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Stdout = os.Stdout
-
+	cmd := newCommand(isolateNamespace, exeCommand)
 	if err := cmd.Start(); err != nil {
-		fmt.Println("ERROR", err)
 		os.Exit(1)
 	} else {
-		fmt.Println(cmd.Process.Pid)
-
-		os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
-		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
-		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"), []byte("500m"), 0644)
+		setCgroupFor(cmd.Process.Pid, cgroupMemoryHierarchyMount)
 	}
 
 	cmd.Process.Wait()
